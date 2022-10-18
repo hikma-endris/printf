58003401c4375a8ba6e444d1e8341d19364a3744
Hikma Endris <hikmuyemenusanu@gmail.com>
2022-10-18 18:30:57 +0300

    Add task 0

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _printf - to print the give character
+ * @format: pointer to a format string
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+}
